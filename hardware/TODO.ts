98603b2938e5ef9e0ee7064843bb2a03570a7006
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-12 14:16:43 +0000

    Added minutes from a check-in with the customer rep.
    Signed-off-by: Andrew Lucas <sqweelygig@googlemail.com>

---
+++ hardware/TODO.ts
@@ -0,0 +1,8 @@
+// TODO [BLOCKER] Museum ready casing
+// TODO [BLOCKER] Focus LED lenses
+// TODO Surface mount case
+// TODO Turntable
+// TODO Music
+// TODO HDMI Video
+// TODO e-ink panel
+// TODO touchscreen kiosk
